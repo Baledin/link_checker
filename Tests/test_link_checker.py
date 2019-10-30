d6749d708ca5bd6f0593522b2458f4ac7d82b98f
Jeremy Stroud <jeremy.stroud@sos.wa.gov>
2019-10-30 14:44:59 -0700

    Renamed unit tests to unit_xx()

--- Tests/test_link_checker.py
+++ Tests/test_link_checker.py
@@ -12,21 +12,21 @@ lc = link_checker
 test_url = "https://www.sos.wa.gov/library"
 
 def main():
-    test_add_link()
-    test_add_url()
-    test_get_error_urls()
-    test_get_header()
-    test_get_page()
-    test_get_urls()
-    test_initialize_db()
-    test_parse_content()
-    test_process_url()
-    test_process_url_status()
-    test_update_url_status()
-    test_validate_url()
+    unit_add_link()
+    unit_add_url()
+    unit_get_error_urls()
+    unit_get_header()
+    unit_get_page()
+    unit_get_urls()
+    unit_initialize_db()
+    unit_parse_content()
+    unit_process_url()
+    unit_process_url_status()
+    unit_update_url_status()
+    unit_validate_url()
 
-def test_add_link():
-    logging.info("***** test_add_link starting *****")
+def unit_add_link():
+    logging.info("***** unit_add_link starting *****")
     # Initialize if not already set
     conn = lc.get_connection(':memory:')
     lc.initialize_db(conn)
@@ -34,7 +34,7 @@ def test_add_link():
     url_id = lc.add_url(test_url, conn)
 
     r = random.randint(3, 10)
-    logging.info("test_add_link looping %d times." % r)
+    logging.info("unit_add_link looping %d times." % r)
     for _ in range(r):
         lc.add_link(url_id, url_id, conn)
 
@@ -46,15 +46,15 @@ def test_add_link():
     # As add_link iterates counter, result should be equal to number of loops
     try:
         assert result[0] == r
-        logging.info("test_add_link passed - %d expected, %d found." % (r, result[0]))
+        logging.info("unit_add_link passed - %d expected, %d found." % (r, result[0]))
     except AssertionError as e:
-        logging.error("test_add_link failed - %d expected, %d found." % (r, result[0]))
+        logging.error("unit_add_link failed - %d expected, %d found." % (r, result[0]))
         logging.debug(e)
 
-    logging.info("***** test_add_link complete *****")
+    logging.info("***** unit_add_link complete *****")
 
-def test_add_url():
-    logging.info("***** test_add_url starting *****")
+def unit_add_url():
+    logging.info("***** unit_add_url starting *****")
     
     # Initialize db if not already set
     conn = lc.get_connection(':memory:')
@@ -65,71 +65,72 @@ def test_add_url():
 
     try:
         assert url_id > 0
-        logging.info("test_add_url passed - %s added to db." % url)
+        logging.info("unit_add_url passed - %s added to db." % url)
     except AssertionError:
-        logging.info("test_add_url failed - %s not found." % url)
+        logging.info("unit_add_url failed - %s not found." % url)
 
     conn.close()
     
-    logging.info("***** test_add_url complete *****")
+    logging.info("***** unit_add_url complete *****")
 
-def test_get_error_urls():
-    logging.info("***** test_get_error_urls starting *****")
+def unit_get_error_urls():
+    logging.info("***** unit_get_error_urls starting *****")
+    assert True
+    logging.info("***** unit_get_error_urls complete *****")
+
+def unit_get_header():
+    logging.info("***** unit_get_header starting *****")
     #TODO
     assert True
-    logging.info("***** test_get_error_urls complete *****")
+    logging.info("***** unit_get_header complete *****")
 
-def test_get_header():
-    logging.info("***** test_get_header starting *****")
+def unit_get_page():
+    logging.info("***** unit_get_page starting *****")
     #TODO
     assert True
-    logging.info("***** test_get_header complete *****")
+    logging.info("***** unit_get_page complete *****")
 
-def test_get_page():
-    logging.info("***** test_get_page starting *****")
+def unit_get_urls():
+    logging.info("***** unit_get_urls starting *****")
     #TODO
     assert True
-    logging.info("***** test_get_page complete *****")
+    logging.info("***** unit_get_urls complete *****")
 
-def test_get_urls():
-    logging.info("***** test_get_urls starting *****")
+def unit_initialize_db():
+    logging.info("***** unit_initialize_db starting *****")
     #TODO
     assert True
-    logging.info("***** test_get_urls complete *****")
+    logging.info("***** unit_initialize_db complete *****")
 
-def test_initialize_db():
-    logging.info("***** test_initialize_db starting *****")
-    #TODO
-    assert True
-    logging.info("***** test_initialize_db complete *****")
-
-def test_parse_content():
-    logging.info("***** test_parse_content starting *****")
+def unit_parse_content():
+    logging.info("***** unit_parse_content starting *****")
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
     }
-    logging.info("***** test_parse_content complete *****")
-
-def test_process_url():
-    logging.info("***** test_process_url starting *****")
     #TODO
     assert True
-    logging.info("***** test_process_url complete *****")
+    logging.info("***** unit_parse_content complete *****")
 
-def test_process_url_status():
-    logging.info("***** test_process_url_status starting *****")
+def unit_process_url():
+    logging.info("***** unit_process_url starting *****")
     #TODO
     assert True
-    logging.info("***** test_process_url_status complete *****")
+    logging.info("***** unit_process_url complete *****")
 
-def test_update_url_status():
-    logging.info("***** test_update_url_status starting *****")
+def unit_process_url_status():
+    logging.info("***** unit_process_url_status starting *****")
     #TODO
     assert True
-    logging.info("***** test_update_url_status complete *****")
+    logging.info("***** unit_process_url_status complete *****")
 
-def test_validate_url():
-    logging.info("***** test_validate_url starting *****")
+def unit_update_url_status():
+    logging.info("***** unit_update_url_status starting *****")
+    #TODO
+    assert True
+    logging.info("***** unit_update_url_status complete *****")
+
+def unit_validate_url():
+    logging.info("***** unit_validate_url starting *****")
     valid_urls = (
         "http://google.com", 
         "http://localhost:8080", 
@@ -158,7 +159,7 @@ def test_validate_url():
         except AssertionError as ex:
             logging.debug(ex)
 
-    logging.info("***** test_validate_url complete *****")
+    logging.info("***** unit_validate_url complete *****")
 
 if __name__ == "__main__":
     main()
