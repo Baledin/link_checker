98f9302811d4bf4dff0d5ca49b91685f4676354c
Jeremy Stroud <jeremy.stroud@sos.wa.gov>
2019-11-04 14:33:54 -0800

    Removed method and unit test for reset

--- Tests/test_link_checker.py
+++ Tests/test_link_checker.py
@@ -12,7 +12,6 @@ lc = link_checker
 test_url = "https://www.sos.wa.gov/library"
 
 def main():
-    unit_reset()
     unit_add_link()
     unit_add_url()
     unit_get_error_urls()
@@ -123,13 +122,6 @@ def unit_process_url_status():
     #TODO
     assert True
     logging.info("***** unit_process_url_status complete *****")
-
-def unit_reset():
-    logging.info("***** unit_reset starting *****")
-    conn = lc.get_connection()
-    lc.initialize_db(conn)
-    lc.reset()
-    logging.info("***** unit_reset complete *****")
 
 def unit_update_url_status():
     logging.info("***** unit_update_url_status starting *****")
